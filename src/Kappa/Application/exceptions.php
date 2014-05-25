f299875926ac09584491134f6733e3c6b30d5857
Budry <zarubaondra@gmail.com>
2014-05-25 21:45:36 +0200

    Added exceptions

---
+++ src/Kappa/Application/exceptions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the Kappa\Application package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\Application;
+
+/**
+ * Class InvalidStateException
+ * @package Kappa\Application
+ */
+class InvalidStateException extends \RuntimeException
+{
+
+}
