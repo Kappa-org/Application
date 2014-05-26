1563b9fd681c680919429570ede7fc1acc251bd0
Budry <zarubaondra@gmail.com>
2014-05-26 19:59:35 +0200

    Added extension for link generator

---
+++ src/Kappa/Application/DI/ApplicationExtension.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the Kappa\Application package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\Application\DI;
+
+use Nette\DI\CompilerExtension;
+
+/**
+ * Class ApplicationExtension
+ * @package Kappa\Application\DI
+ */
+class ApplicationExtension extends CompilerExtension
+{
+	public function loadConfiguration()
+	{
+		$builder = $this->getContainerBuilder();
+
+		$builder->addDefinition($this->prefix('linkGenerator'))
+			->setClass('Kappa\Application\Helpers\LinkGenerator');
+	}
+}
