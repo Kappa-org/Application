6b48492a820a94de16bb7614b3942e2647c9caa1
Budry <zarubaondra@gmail.com>
2014-06-22 21:18:06 +0200

    Added support for counted moudule <module1>, <module2>, atc...

--- src/Kappa/Application/PresenterFactory.php
+++ src/Kappa/Application/PresenterFactory.php
@@ -10,6 +10,10 @@
 
 namespace Kappa\Application;
 
+use Nette\Application\InvalidPresenterException;
+use Nette\Reflection\ClassType;
+use Nette\Utils\Strings;
+
 /**
  * Class PresenterFactory
  * @package Kappa\Application
@@ -19,6 +23,9 @@ class PresenterFactory extends \Nette\Application\PresenterFactory
 	/** @var array */
 	private $mapping = array();
 
+	/** @var array */
+	private $cache = array();
+
 	/**
 	 * @param array $mapping
 	 * @return $this|\Nette\Application\PresenterFactory
@@ -27,7 +34,9 @@ class PresenterFactory extends \Nette\Application\PresenterFactory
 	public function setMapping(array $mapping)
 	{
 		foreach ($mapping as $module => $mask) {
-			if (!preg_match('~^\\\\?((\w+)?(<presenter>|<module>)?(\w+)?\\\\?)+((\w+)?(<presenter>|<module>)?(\w+)?)$~', $mask, $m)) {
+			if (!preg_match('~^\\\\?((\w+)?(<presenter>|<module([1-9]{1}[0-9]*)?>)?(\w+)?\\\\?)+((\w+)?(<presenter>|<module([1-9]{1}[0-9]*)?>)?(\w+)?)$~', $mask, $m)
+				|| (strpos($mask, '<module>') !== false && preg_match('~<module[1-9]{1}[0-9]*>~', $mask))
+			) {
 				throw new InvalidStateException("Invalid mapping mask '$mask'.");
 			}
 			$this->mapping[$module] = $m[0];
@@ -46,16 +55,27 @@ class PresenterFactory extends \Nette\Application\PresenterFactory
 		$parts = explode(':', $presenter);
 		$presenterName = end($parts);
 		unset($parts[count($parts) - 1]);
-		$mapping = isset($parts[1], $this->mapping[$parts[0]])
+		$mask = isset($parts[1], $this->mapping[$parts[0]])
 			? $this->mapping[array_shift($parts)]
 			: $this->mapping['*'];
-		$mapping = str_replace('<presenter>', $presenterName, $mapping);
-		$count = substr_count($mapping, '<module>');
-		if ($count != count($parts)) {
-			throw new InvalidStateException("No mapping mask for '$presenter'");
-		}
-		foreach ($parts as $module) {
-			$mapping = preg_replace('~<module>~', $module, $mapping, 1);
+		$mapping = str_replace('<presenter>', $presenterName, $mask);
+		if (strpos($mapping, '<module>')) {
+			$count = substr_count($mapping, '<module>');
+			if ($count != count($parts)) {
+				throw new InvalidStateException("No mapping mask for '$presenter'");
+			}
+			foreach ($parts as $module) {
+				$mapping = preg_replace('~<module>~', $module, $mapping, 1);
+			}
+		} else {
+			if (preg_match_all('~<module([1-9]{1}[0-9]*)>~i', $mapping, $matches)) {
+				foreach ($matches[0] as $key => $pattern) {
+					$mapping = str_replace($pattern, $parts[$matches[1][$key] - 1], $mapping);
+				}
+				if (preg_match('~<module[1-9]{1}[0-9]*>~i', $mapping)) {
+					throw new InvalidStateException("Mask '{$mask}' is not compatible with '{$presenter}' name");
+				}
+			}
 		}
 
 		return $mapping;
@@ -68,12 +88,88 @@ class PresenterFactory extends \Nette\Application\PresenterFactory
 	 */
 	public function unformatPresenterClass($class)
 	{
-		foreach ($this->mapping as $module => $mapping) {
-			$mapping = str_replace(array('\\', '<presenter>', '<module>'), array('\\\\', '(\w+)', '(\w+)'), $mapping);
-			if (preg_match("~$mapping~", $class, $matches)) {
+		$presenters = array();
+		foreach ($this->mapping as $module => $mask) {
+			$replace = array(
+				'~\\\\~' => '\\\\\\\\',
+				'~<presenter>~' => '(\w+)',
+				'~<module([1-9]{1}[0-9]*)?>~' => '(\w+)'
+			);
+			$mapping = preg_replace(array_keys($replace), array_values($replace), $mask);
+			if (preg_match("~$mapping~i", $class, $matches)) {
 				unset($matches[0]);
-				return ($module === '*' ? '' : $module . ':') . implode(':', $matches);
+				if (preg_match_all('~<module([1-9]{1}[0-9]*)>~i', $mask, $order)) {
+					asort($order[1]);
+					$sortedMatches = array();
+					foreach ($order[1] as $index => $position) {
+						$sortedMatches[] = $matches[$index + 1];
+						unset($matches[$index + 1]);
+					}
+
+					$matches = array_merge($sortedMatches, $matches);
+				}
+				$presenters[] = ($module === '*' ? '' : $module . ':') . implode(':', $matches);
 			}
 		}
+
+		return $presenters;
+	}
+
+	/**
+	 * Generates and checks presenter class name.
+	 * @param  string  presenter name
+	 * @return string  class name
+	 * @throws InvalidPresenterException
+	 */
+	public function getPresenterClass(& $name)
+	{
+		if (isset($this->cache[$name])) {
+			list($class, $name) = $this->cache[$name];
+			return $class;
+		}
+
+		if (!is_string($name) || !Strings::match($name, '#^[a-zA-Z\x7f-\xff][a-zA-Z0-9\x7f-\xff:]*\z#')) {
+			throw new InvalidPresenterException("Presenter name must be alphanumeric string, '$name' is invalid.");
+		}
+
+		$class = $this->formatPresenterClass($name);
+
+		if (!class_exists($class)) {
+			// internal autoloading
+			$file = $this->formatPresenterFile($name);
+			if (is_file($file) && is_readable($file)) {
+				call_user_func(function() use ($file) { require $file; });
+			}
+
+			if (!class_exists($class)) {
+				throw new InvalidPresenterException("Cannot load presenter '$name', class '$class' was not found in '$file'.");
+			}
+		}
+
+		$reflection = new ClassType($class);
+		$class = $reflection->getName();
+
+		if (!$reflection->implementsInterface('Nette\Application\IPresenter')) {
+			throw new InvalidPresenterException("Cannot load presenter '$name', class '$class' is not Nette\\Application\\IPresenter implementor.");
+		}
+
+		if ($reflection->isAbstract()) {
+			throw new InvalidPresenterException("Cannot load presenter '$name', class '$class' is abstract.");
+		}
+
+		// canonicalize presenter name
+		$realNames = $this->unformatPresenterClass($class);
+		if ($this->caseSensitive) {
+			$index = array_search(strtolower($name), array_map('strtolower', $realNames));
+		} else {
+			$index = array_search($name, $realNames);
+		}
+		if ($index !== false) {
+			$realName = $realNames[$index];
+			$this->cache[$name] = array($class, $realName);
+			$name = $realName;
+		}
+
+		return $class;
 	}
 } 
--- tests/Kappa/Application/PresenterFactoryTest.phpt
+++ tests/Kappa/Application/PresenterFactoryTest.phpt
@@ -17,6 +17,8 @@ use Kappa\Tester\MockTestCase;
 use Tester\Assert;
 
 require_once __DIR__ . '/../bootstrap.php';
+require_once __DIR__ . '/../../data/TestPresenter2.php';
+require_once __DIR__ . '/../../data/TestPresenter.php';
 
 /**
  * Class PresenterFactoryTest
@@ -40,28 +42,51 @@ class PresenterFactoryTest extends MockTestCase
 			'App\<module>Module\Presenters\<presenter>Presenter',
 			'App\<module>\Presenters\Presenter<presenter>',
 			'App',
-			'\<module>App'
+			'\<module>App',
+			'<module1>\<module2>'
 		);
 		Assert::type(get_class($this->presenterFactory), $this->presenterFactory->setMapping($mapping));
 		Assert::throws(function () {
 			$this->presenterFactory->setMapping(array('App\*Module\Presenters\*Presenter'));
 		}, 'Kappa\Application\InvalidStateException');
+		Assert::throws(function () {
+			$this->presenterFactory->setMapping(array('<module>\<module1>'));
+		}, 'Kappa\Application\InvalidStateException');
 	}
 
 	public function testFormatPresenterClass()
 	{
-		Assert::type(get_class($this->presenterFactory), $this->presenterFactory->setMapping(array('*' => 'App\<module>Module\Presenters\<module>\<presenter>Presenter')));
-		$expected = 'App\TestModule\Presenters\Sub\TestPresenter';
-		$presenter = 'Test:Sub:Test';
-		Assert::same($expected, $this->presenterFactory->formatPresenterClass($presenter));
+		$mapping = array(
+			'Type1' => 'App\<module>Module\Presenters\<module>\<presenter>Presenter',
+			'Type2' => 'App\<module2>Module\Presenters\<module1>\<presenter>Presenter'
+		);
+		Assert::type(get_class($this->presenterFactory), $this->presenterFactory->setMapping($mapping));
+		Assert::same('App\TestModule\Presenters\Sub\TestPresenter', $this->presenterFactory->formatPresenterClass('Type1:Test:Sub:Test'));
+		Assert::same('App\SubModule\Presenters\Test\TestPresenter', $this->presenterFactory->formatPresenterClass('Type2:Test:Sub:Test'));
 	}
 
 	public function testUnformatPresenterClass()
 	{
-		Assert::type(get_class($this->presenterFactory), $this->presenterFactory->setMapping(array('*' => 'App\<module>Module\Presenters\<module>\<presenter>Presenter')));
-		$expected = 'Test:Sub:Test';
-		$class = 'App\TestModule\Presenters\Sub\TestPresenter';
-		Assert::same($expected, $this->presenterFactory->unformatPresenterClass($class));
+		$mapping = array(
+			'Type1' => 'App\<module>Module\Presenters\<module>\<presenter>Presenter',
+			'Type2' => 'App\<module2>Module\Presenters\<module1>\<presenter>Presenter'
+		);
+		Assert::type(get_class($this->presenterFactory), $this->presenterFactory->setMapping($mapping));
+		Assert::true(in_array('Type1:Test:Sub:Test', $this->presenterFactory->unformatPresenterClass('App\TestModule\Presenters\Sub\TestPresenter')));
+		Assert::true(in_array('Type2:Test:Sub:Test', $this->presenterFactory->unformatPresenterClass('App\SubModule\Presenters\Test\TestPresenter')));
+	}
+
+	public function testGetPresenterClass()
+	{
+		$mapping = array(
+			'Type1' => 'App\<module>Module\Presenters\<module>\<presenter>Presenter',
+			'Type2' => 'App\<module2>Module\Presenters\<module1>\<presenter>Presenter'
+		);
+		Assert::type(get_class($this->presenterFactory), $this->presenterFactory->setMapping($mapping));
+		$name1 = 'Type1:Test:Sub:Test';
+		$name2 = 'Type2:test:sub:test';
+		Assert::same('App\TestModule\Presenters\Sub\TestPresenter', $this->presenterFactory->getPresenterClass($name1));
+		Assert::same('App\SubModule\Presenters\Test\TestPresenter', $this->presenterFactory->getPresenterClass($name2));
 	}
 }
 
