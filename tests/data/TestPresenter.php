78c9d9b84445d358690874126bf7b56931f96637
Budry <zarubaondra@gmail.com>
2014-06-22 21:17:31 +0200

    Added presenters for tests

---
+++ tests/data/TestPresenter.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the Kappa\Application package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace App\SubModule\Presenters\Test;
+
+use Nette\Application\UI\Presenter;
+
+class TestPresenter extends Presenter
+{
+}
---
+++ tests/data/TestPresenter2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the Kappa\Application package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace App\TestModule\Presenters\Sub;
+
+use Nette\Application\UI\Presenter;
+
+class TestPresenter extends Presenter
+{
+
+} 
